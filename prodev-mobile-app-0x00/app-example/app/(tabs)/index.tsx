f2455c5131e85b7be69e46be10347e8fa12011ce
Jemimah Jemutai <jemutaijemimah@gmail.com>
2026-01-11 11:57:47 +0300

    Create index.tsx

---
+++ prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>First App Created</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
